984cc696280434b168a7a4741bb22a7a30f8b9da
Harshul <harshulnarang@ymail.com>
2026-04-18 21:55:22 +0100

    Initial Commit

---
+++ MicroAdventures/MicroAdventuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroAdventuresApp.swift
+//  MicroAdventures
+//
+//  Created by Harshul on 18/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MicroAdventuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
